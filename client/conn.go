f1425c5a9efafb83dfc351eeacc8b74cf30530d5
B <b@xbug.io>
2019-07-25 21:51:19 +0800

    解决错误调用问题

--- client/conn.go
+++ client/conn.go
@@ -15,40 +15,40 @@ type Conn struct {
 
 // Read 从proxy中读取数据
 func (c *Conn) Read(b []byte) (n int, err error) {
-	return c.Read(b)
+	return c.conn.Read(b)
 }
 
 // Write 往proxy中写数据
 func (c *Conn) Write(b []byte) (n int, err error) {
-	return c.Write(b)
+	return c.conn.Write(b)
 }
 
 // Close 关闭proxy连接
 func (c *Conn) Close() error {
-	return c.Close()
+	return c.conn.Close()
 }
 
 // LocalAddr 返回本地地址
 func (c *Conn) LocalAddr() Addr {
-	return c.LocalAddr()
+	return c.conn.LocalAddr()
 }
 
 // RemoteAddr 返回远程地址
 func (c *Conn) RemoteAddr() Addr {
-	return c.RemoteAddr()
+	return c.conn.RemoteAddr()
 }
 
 // SetDeadline 设置读写超时时间，零值为不限
 func (c *Conn) SetDeadline(t time.Time) error {
-	return c.SetDeadline(t)
+	return c.conn.SetDeadline(t)
 }
 
 // SetReadDeadline 设定该连接的读操作deadline，参数t为零值表示不设置期限
 func (c *Conn) SetReadDeadline(t time.Time) error {
-	return c.SetReadDeadline(t)
+	return c.conn.SetReadDeadline(t)
 }
 
 // SetWriteDeadline 设定该连接的写操作deadline，参数t为零值表示不设置期限
 func (c *Conn) SetWriteDeadline(t time.Time) error {
-	return c.SetWriteDeadline(t)
+	return c.conn.SetWriteDeadline(t)
 }
